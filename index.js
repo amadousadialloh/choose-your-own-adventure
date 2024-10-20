328562a967f37cfca785b89f2a8b45900f36e493
amadousadialloh <amadousadialloh@gmail.com>
2024-10-20 15:09:05 -0500

    Choose your own adventure Exercise completed

---
+++ index.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")();
+
+//choose your own adventure
+
+//Number gessing game
+
+//Random number generator from 1 to 100
+const randomNumberGenerator = () => {
+  const guessNumber = Math.floor(Math.random() * 20) + 1;
+  return guessNumber;
+};
+
+//Game logic
+function guessingGameLogic() {
+  //Ask the user to enter the number
+  let guess = prompt("Guess a number between 1 and 20: ");
+
+  guess = parseInt(guess); //convert it to number
+  const random = randomNumberGenerator();
+  if (guess === random) {
+    console.log("Congratulations, you guessed the correct number");
+  } else {
+    console.log("wrong answer");
+  }
+}
+
+guessingGameLogic();
